9077f8641f644dc55d9c9b7d95accc1644a31de4
jlopez16 <j.lopez16@outlook.com>
2018-05-14 18:11:01 -0500

    Modificacion del cerrrar sesion, y agregar boton agregar proyectos-usuario

--- SGPF/src/main/java/unam/mx/SGPF/cerrarSesion.java
+++ SGPF/src/main/java/unam/mx/SGPF/cerrarSesion.java
@@ -1,14 +1,11 @@
 package unam.mx.SGPF;
 
 import java.io.IOException;
-import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import unam.mx.SGPF.model.InterUP;
-import unam.mx.SGPF.model.Usuario;
 
 public class cerrarSesion extends HttpServlet {
     @Override
